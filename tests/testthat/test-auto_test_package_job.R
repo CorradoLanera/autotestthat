95269bac54b3cde7bff50b83edfd08dd4130c3af
Corrado <corrado.lanera@gmail.com>
2019-07-19 21:29:28 -0400

    functions copied from my testthat fork

---
+++ tests/testthat/test-auto_test_package_job.R
@@ -0,0 +1,47 @@
+test_that("auto_test_package_job throw expected errors", {
+    if (!requireNamespace("rstudioapi")) {
+        expect_error(auto_test_package_job(), "rstudioapi required")
+    } else if (!rstudioapi::isAvailable("1.2")) {
+        expect_error(
+            auto_test_package_job(),
+            "Need at least|RStudio not running"
+        )
+    } else {
+        expect_error(auto_test_package_job(1), "invalid 'path'")
+    }
+})
+
+
+test_that("stop_auto_test_job throw expected errors", {
+
+    if (!requireNamespace("rstudioapi")) {
+        expect_error(stop_auto_test_job(), "rstudioapi required")
+
+
+    } else if (!rstudioapi::isAvailable("1.2")) {
+        expect_error(stop_auto_test_job(),
+                     "Need at least|RStudio not running"
+        )
+
+
+    } else if (can_stop_jobs()) {
+        expect_error(stop_auto_test_job(1), "does not exists")
+
+    } else {
+        expect_warning(stop_auto_test_job(),
+                       "jobs cannot be stopped automatically"
+        )
+
+        expect_message(suppressWarnings(stop_auto_test_job()),
+                       "Jobs can be stopped manually"
+        )
+    }
+})
+
+test_that("auto_test_package throw expected error", {
+    expect_error(auto_test_package(1), "must be a string")
+    expect_error(auto_test_package(reporter = 1), "Invalid input")
+    expect_error(
+        auto_test_package("abcdefghijklmnopqrstuzv"),
+        "Can't find")
+})
